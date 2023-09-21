704bed1f8100292f10cc9e3a4640cba4453b11c8
lavish <lavishjain36@gmail.com>
2023-09-21 21:58:53 +0530

    code

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,7 +1,55 @@
 import React from "react";
+import {Link} from "react-router-dom";
+import image from "../images/lmsimage.jpeg";
 const Header=()=>{
     return(
-        <h1>Header Component</h1>
+        <header class="text-gray-600 body-font">
+        <div class="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+
+          <Link class="flex title-font font-medium items-center text-gray-900 mb-4 md:mb-0"
+            to="/"
+          >
+           
+            <svg xmlns="http://www.w3.org/2000/svg" fill="none" stroke="currentColor" stroke-linecap="round" stroke-linejoin="round" stroke-width="2" class="w-10 h-10 text-white p-2 bg-indigo-500 rounded-full" viewBox="0 0 24 24">
+              <path d="M12 2L2 7l10 5 10-5-10-5zM2 17l10 5 10-5M2 12l10 5 10-5"></path>
+            </svg>
+            <img src={image} alt="lms" style={{width:"120px"}}/>
+            <span class="ml-3 text-xl">E-Learning</span>
+          </Link>
+          <nav class="md:ml-auto md:mr-auto flex flex-wrap items-center text-base justify-center">
+            <Link class="mr-5 hover:text-gray-900" to="/">
+                Home
+            </Link>
+            <Link class="mr-5 hover:text-gray-900" to="/about">
+                About
+            </Link>
+            <Link class="mr-5 hover:text-gray-900" to="/courses">
+                Explore Course
+            </Link>
+            <Link class="mr-5 hover:text-gray-900" to="/createcourse">
+                Create Course
+            </Link>
+
+            <Link class="mr-5 hover:text-gray-900" to="/contact">
+                Contact
+            </Link>
+          </nav>
+          <Link to="/register">
+          <button class="inline-flex items-center rounded-md text-white bg-blue-500 border-0 py-1 px-3 focus:outline-none hover:bg-blue-600 rounded text-base mt-4 md:mt-0">
+            Sign Up
+          </button>
+          </Link>
+
+
+          <Link to="/login">
+          <button class="inline-flex items-center rounded-md text-white bg-orange-500 border-0 py-1 px-3 focus:outline-none hover:bg-orange-600 rounded text-base mt-4 md:mt-0">
+            Login
+          </button>
+          </Link>
+         
+        </div>
+      </header>
+      
     )
 }
 
--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,7 +1,30 @@
 import React from "react";
 const Hero=()=>{
     return(
-        <h1>Hero Component</h1>
+    <section className="text-gray-600 body-font">
+        <div className="container mx-auto flex px-5 py-24 md:flex-row flex-col items-center">
+            <div className="lg:flex-grow md:w-1/2 lg:pr-24 md:pr-16 flex flex-col md:items-start md:text-left mb-16 md:mb-0 items-center text-center">
+                <h1 className="title-font sm:text-4xl text-3xl mb-4 mt-16 font-medium text-gray-900">
+                    Master Skills &
+                    <br className="hidden lg:inline-block" />
+                    Learn from Experts
+                </h1>
+                <p className="mb-8 leading-relaxed">
+                    Our Online Learning Platform offers expert-led courses
+                    to master new skills. Join us now to achieve your goals
+                    through interactive methods, 24/7 access and community
+                    support.
+                </p>
+            </div>
+            <div className="lg:max-w-lg lg:w-full md:w-1/2 w-5/6">
+                <img
+                    className="object-cover object-center rounded"
+                    alt="hero"
+                    src="https://img.freepik.com/free-vector/students-using-e-learning-platform-video-laptop-graduation-cap-online-education-platform-e-learning-platform-online-teaching-concept_335657-795.jpg?w=900&t=st=1688369245~exp=1688369845~hmac=d28ece6242af926baaf64d190c23e77d5e136a63601403636082853c9099c960"
+                />
+            </div>
+        </div>
+</section>
     )
 }
 
